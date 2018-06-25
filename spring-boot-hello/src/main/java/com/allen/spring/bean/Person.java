abca8b1a04738b0adfe7c8ec2faed76d69091343
ligenfeng <ligenfeng@chinasofti.com>
2018-06-25 11:34:15 +0800

    add spring boot configuration

---
+++ spring-boot-hello/src/main/java/com/allen/spring/bean/Person.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.allen.spring.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author first
+ * 获取自定义配置文件.
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
--- spring-boot-hello/src/main/java/com/allen/spring/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/allen/spring/controller/HelloController.java
@@ -3,9 +3,12 @@
  */
 package com.allen.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.allen.spring.bean.Person;
 
 /**
  * @author first
@@ -15,8 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/hello")
 public class HelloController {
 
+    
+    @Autowired
+    private Person person;
+    
     @GetMapping
     public String hello() {
-        return "hello boot";
+        return person.getName();
     }
 }
