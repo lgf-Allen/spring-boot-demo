f797515f65005b22078c34df826920a830414e43
ligenfeng <ligenfeng@chinasofti.com>
2018-07-22 21:19:20 +0800

    update

---
+++ spring-boot-was/src/main/java/com/allen/spring/SpringBootWasApplication.java
@@ -0,0 +1,12 @@
+package com.allen.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWasApplication.class, args);
+	}
+}
