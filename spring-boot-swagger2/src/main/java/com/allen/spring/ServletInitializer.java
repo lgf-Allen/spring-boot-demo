c412a203295c095dfb325c7a47f6e249bd0a3cf7
ligenfeng <ligenfeng@chinasofti.com>
2018-08-13 13:44:28 +0800

    update spring-boot-swagger2 boot.version-->2.0.3.RELEASE

--- spring-boot-swagger2/src/main/java/com/allen/spring/ServletInitializer.java
+++ spring-boot-swagger2/src/main/java/com/allen/spring/ServletInitializer.java
@@ -1,9 +1,8 @@
 package com.allen.spring;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
--- spring-boot-swagger2/src/main/java/com/allen/spring/config/Swagger2Configuration.java
+++ spring-boot-swagger2/src/main/java/com/allen/spring/config/Swagger2Configuration.java
@@ -5,10 +5,6 @@ package com.allen.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
