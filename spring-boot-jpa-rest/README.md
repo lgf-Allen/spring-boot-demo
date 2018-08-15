7a6fb29a492e8b9e7ef25ef3937ae41a7669312f
ligenfeng <ligenfeng@chinasofti.com>
2018-08-15 16:54:32 +0800

    add new project:spring-boot-jpa-rest

---
+++ spring-boot-jpa-rest/README.md
@@ -0,0 +1,2 @@
+# Spring-data-jpa with Rest
+[Spring-data-jpa with REST](https://spring.io/guides/gs/accessing-data-rest/)
---
+++ spring-boot-jpa-rest/src/main/java/com/allen/spring/repository/PersonRepository.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.allen.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.allen.spring.bean.Person;
+
+/**
+ * @author first
+ *
+ */
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+}
