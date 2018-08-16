a5646339182d28eee1fae35b4dddfc15436adc29
allen <15829927394@163.com>
2018-08-16 23:42:09 +0800

    update README.md

---
+++ spring-boot-jetty/src/main/java/com/allen/spring/boot/SpringBootJettyApplication.java
@@ -0,0 +1,12 @@
+package com.allen.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJettyApplication.class, args);
+	}
+}
