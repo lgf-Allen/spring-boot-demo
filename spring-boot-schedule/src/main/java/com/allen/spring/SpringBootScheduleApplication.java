84c11025c3ebc8dd0a7f3c8673189c80efcccdc2
ligenfeng <ligenfeng@chinasofti.com>
2018-08-22 16:14:35 +0800

    add JDK Timer schedule sample.

--- spring-boot-schedule/src/main/java/com/allen/spring/SpringBootScheduleApplication.java
+++ spring-boot-schedule/src/main/java/com/allen/spring/SpringBootScheduleApplication.java
@@ -1,5 +1,8 @@
 package com.allen.spring;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootScheduleApplication {
 
 	public static void main(String[] args) {
+	    //timerSample();//开启timer测试
 		SpringApplication.run(SpringBootScheduleApplication.class, args);
+		
+	}
+	
+	/**
+	 * JDK Timer Sample. 
+	 */
+	public static void timerSample() {
+	    Timer timer = new Timer("MyTimer");
+        timer.schedule(new TimerTask() {
+            int i = 0 ;
+            @Override
+            public void run() {
+                ++ i;
+                System.out.println(" Timer execute "+ i +"次,Now timeStamp is "+ System.currentTimeMillis());
+            }
+        },  2000l , 3000l);
 	}
 }
