28000530ee74e5c3fed9d0d7ecf94314d89166d7
ligenfeng <ligenfeng@chinasofti.com>
2018-08-27 14:33:25 +0800

    add @Valid annotation instruction

--- spring-boot-hello/src/main/java/com/allen/spring/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/allen/spring/controller/HelloController.java
@@ -4,6 +4,7 @@
 package com.allen.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,17 @@ public class HelloController {
     @Autowired
     private Person person;
     
+    /**
+     * '@Value'支持两种表达式
+     * 1.PlaceHolder方式：@value("${person.age}")
+     * 2.Spring Expression Language：@value("#{person.age}")
+     */
+    @Value("#{person.age}")
+    private int age;
+    
     @GetMapping(path="/hello")
     public String hello() {
+        System.out.println(age);
         return person.getName();
     }
 }
