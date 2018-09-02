d25d4f538bb5599795c3489bcd050af1e4c7e5d5
allen <15829927394@163.com>
2018-09-02 22:50:37 +0800

    add thread project

---
+++ spring-boot-thread/src/main/java/com/allen/spring/boot/thread/ThreadSleep.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.allen.spring.boot.thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author meng
+ *
+ */
+public class ThreadSleep {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		new Thread(() -> {
+			long startTime = System.currentTimeMillis();
+			sleep(2L);
+			long endTime = System.currentTimeMillis();
+			System.out.println(String.format("Total spend %d ms", (endTime - startTime)));
+			
+		}).start();
+		
+		long startTime =  System.currentTimeMillis();
+		sleep(3L);
+		long endTime = System.currentTimeMillis();
+		System.out.println(String.format("Main thread total spend %d ms", (endTime - startTime)));
+	}
+
+	private static void sleep(long l) {
+
+		try {
+//			Thread.sleep(l);
+			TimeUnit.SECONDS.sleep(l);
+		}catch(InterruptedException e) {
+			
+		}
+	}
+	
+		
+
+}
---
+++ spring-boot-thread/src/main/java/com/allen/spring/boot/thread/ThreadYield.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.allen.spring.boot.thread;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author meng
+ *
+ */
+public class ThreadYield {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		IntStream.range(0, 2).mapToObj(ThreadYield::create).forEach(Thread::start);
+	}
+	private static Thread create(int index) {
+		return new Thread(() ->  {
+			if(index == 0) {
+				Thread.yield();
+			}
+			System.out.println(index);
+		});
+	}
+
+}
