cab44606254758d68b47ac572d6479599ca0ec1d
Allen <lingfeng.allen@gmail.com>
2019-04-21 23:29:58 +0800

    修改org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory-->org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;

--- spring-boot-https/src/main/java/com/allen/spring/boot/config/HttpsConfig.java
+++ spring-boot-https/src/main/java/com/allen/spring/boot/config/HttpsConfig.java
@@ -7,8 +7,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,8 +19,8 @@ import org.springframework.context.annotation.Configuration;
 public class HttpsConfig {
 
 	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	public TomcatServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 			@Override
 			protected void postProcessContext(Context context) {
 				SecurityConstraint securityConstraint = new SecurityConstraint();
